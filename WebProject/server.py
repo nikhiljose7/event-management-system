e4da712d8592f9f7aaa72a5c076280f4309368c4
RoshanVarghese <roshan.varghese5801@gmail.com>
2024-11-26 16:20:05 +0530

    Style File Creation

--- WebProject/server.py
+++ WebProject/server.py
@@ -1,2 +1,17 @@
 # Enter the server code
-print("World")
+from flask import *
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'   # MySQL username
+app.config['MYSQL_PASSWORD'] = '!QazmlP)9021'  # MySQL password
+app.config['MYSQL_DB'] = 'FullStackWebDevelopment'  # Database name
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+app.run(debug=True)
+
